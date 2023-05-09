fc45b57f2b12943b1981ce2e04016970051e78bd
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-05-09 16:01:21 +0500

    changes

--- dist/index.js
+++ dist/index.js
@@ -27,69 +27,53 @@ app.get('/', (req, res) => {
 });
 app.post("/login", async (req, res) => {
     const { username, password } = req.body;
-    const user = await dataBase('login').select('id').where({ username, password }).first();
-    if (user !== undefined) {
-        const token = jwt.sign({ id: user.id }, 'secret');
-        user && res.json({ token, userId: user.id });
-    }
-    else {
-        !user && res.status(401).json({ message: 'Invalid credentials' });
-    }
-});
-app.post('/register', async (req, res) => {
-    const { username, password, firstname, lastname } = req.body;
-    bcrypt.genSalt(10, function (err, salt) {
-        bcrypt.hash(password, salt, async (err, password) => {
-            const userId = await dataBase('login').insert({ username, password, firstname, lastname }, 'id');
-            const token = jwt.sign({ userId }, 'secret');
-            userId && res.json({ token, userId });
-            !userId && res.status(401).json("something went wrong");
-            await dataBase('posts').insert({ login_id: userId[0].id });
+    const hashPassword = await dataBase('login').select('password').where({ username }).first();
+    bcrypt.compare(password, hashPassword.password, (err, result) => {
+        dataBase.select('id').from('login').where({ username }).then((userId) => {
+            console.log(userId);
+            if (result) {
+                const token = jwt.sign({ id: userId[0].id }, 'secret');
+                res.json({ token, userId: userId[0].id });
+            }
+            else {
+                res.status(401).json({ message: 'Invalid credentials' });
+            }
         });
     });
 });
-app.post('/post', (req, res) => {
-    const { media, userName } = req.body;
-    ``;
-    dataBase('post')
-        .returning('*')
-        .insert({
-        date: new Date(),
-        media: media,
-        username: userName,
-        likes: 0
-    }).then((response) => {
-        res.json(response);
+app.post('/register', async (req, res) => {
+    const { username, password, firstname, lastname } = req.body;
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, password) => {
+            await dataBase('login').insert({ username, password, firstname, lastname });
+            dataBase.select('id').from('login').where({ username }).then((userId) => {
+                const token = jwt.sign({ id: userId[0].id }, 'secret');
+                userId && res.json({ token, userId });
+                !userId && res.status(401).json("something went wrong");
+                dataBase('posts').insert({ login_id: userId[0].id });
+            });
+        });
     });
 });
 function verifyToken(req, res, next) {
     const token = req.headers.authorization.split(' ')[1];
     jwt.verify(token, 'secret', (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ message: 'Invalid token' });
-        }
+        err && res.status(401).json({ message: 'Invalid token' });
         req.decoded = decoded;
         next();
     });
 }
 const upload = multer({ dest: 'uploads/' });
-app.put('/api/post/:login_id', upload.single('content'), verifyToken, (req, res) => {
-    // const {comment} = req.body;
-    // const {content} = req.file;
-    // const login_id = req.params.login_id;
-    // console.log("comment", comment)
-    // console.log("content", content)
-    // console.log("loginId", login_id)
-    const { comment } = req.body;
-    const imageBuffer = req.file.path;
-    console.log(comment);
-    console.log(imageBuffer);
-    //  dataBase('posts').where(login_id).update({ comment, content })
-    //   .then(() => {
-    //     res.status(200).json({ message: 'Item updated successfully' });
-    //   })
-    //   .catch((err: any) => {
-    //     console.error(err);
-    //     res.status(500).json({ message: 'Error updating item' });
-    //   });
+app.put('/api/post/:id', upload.single('content'), verifyToken, (req, res) => {
+    const login_id = req.params.id;
+    const { comment, content } = req.body;
+    console.log(comment, content);
+    dataBase('posts').where({ login_id: login_id }).update({ comment, content })
+        .then(() => {
+        res.status(200).json({ message: 'Item updated successfully' });
+    })
+        .catch((err) => {
+        console.error(err);
+        res.status(500).json({ message: 'Error updating item' });
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -34,77 +34,66 @@ app.get('/', (req: any, res: any) => {
 app.post("/login", async (req: any, res: any) => {
   const { username, password } = req.body;
 
-  const user = await dataBase('login').select('id').where({ username, password }).first()
+  const hashPassword = await dataBase('login').select('password').where({ username }).first()
 
-  if(user !== undefined){
-    const token = jwt.sign({ id: user.id }, 'secret');
-    user && res.json({ token, userId: user.id });
-  }else {
-    !user && res.status(401).json({ message: 'Invalid credentials' })
-  }
-  
+  bcrypt.compare(password, hashPassword.password, (err: any, result: any) => {
+
+    dataBase.select('id').from('login').where({ username }).then((userId: Object | any) => {
+      console.log(userId);
+      if(result){
+      const token = jwt.sign({ id: userId[0].id }, 'secret')
+      res.json({ token, userId: userId[0].id });
+    }else{
+      res.status(401).json({ message: 'Invalid credentials' })
+    }
+    })
+  });
+
 });
 
 app.post('/register', async (req: any, res: any) => {
+
   const {username, password, firstname, lastname} = req.body;
 
-  bcrypt.genSalt(10, function(err: any, salt: any) {
+  bcrypt.genSalt(10, (err: any, salt: any) =>  {
     bcrypt.hash(password, salt, async(err: any, password: any) =>  {
-      const userId = await dataBase('login').insert({ username, password, firstname, lastname }, 'id');
-      const token = jwt.sign({ userId }, 'secret');
-      userId && res.json({ token, userId });
-      !userId && res.status(401).json("something went wrong");
-      await dataBase('posts').insert({login_id: userId[0].id })
+
+      await dataBase('login').insert({ username, password, firstname, lastname });
+
+      dataBase.select('id').from('login').where({ username }).then((userId: Object | any) => {
+        const token =  jwt.sign({ id: userId[0].id }, 'secret');
+        userId && res.json({ token, userId });
+        !userId && res.status(401).json("something went wrong");
+        dataBase('posts').insert({login_id: userId[0].id })
+      })
     });
   });
 })
 
-app.post('/post', (req: any, res: any) => {
-  const {media, userName} = req.body;``
-  dataBase('post')
-   .returning('*')
-   .insert({
-    date: new Date(),
-    media: media,
-    username: userName,
-    likes: 0
-  }).then((response: any) => {
-    res.json(response)
-  })
-})
 
 function verifyToken(req: any, res: any, next: any) {
   const token = req.headers.authorization.split(' ')[1];
 
   jwt.verify(token, 'secret', (err: any, decoded: any) => {
-    if (err) {
-      return res.status(401).json({ message: 'Invalid token' });
-    }
+    err && res.status(401).json({ message: 'Invalid token' });
     req.decoded = decoded;
     next();
   });
 }
 const upload = multer({ dest: 'uploads/' });
 
+app.put('/api/post/:id', upload.single('content'), verifyToken, (req: any, res: any) => {
 
-app.put('/api/post/:login_id', upload.single('content'), verifyToken, (req: any, res: any) => {
-  // const {comment} = req.body;
-  // const {content} = req.file;
-  // const login_id = req.params.login_id;
-  // console.log("comment", comment)
-  // console.log("content", content)
-  // console.log("loginId", login_id)
-  const { comment } = req.body; 
-  const imageBuffer = req.file.path; 
-  console.log(comment)
-  console.log(imageBuffer)
-
-  //  dataBase('posts').where(login_id).update({ comment, content })
-  //   .then(() => {
-  //     res.status(200).json({ message: 'Item updated successfully' });
-  //   })
-  //   .catch((err: any) => {
-  //     console.error(err);
-  //     res.status(500).json({ message: 'Error updating item' });
-  //   });
+  const login_id = req.params.id;
+  const { comment, content } = req.body; 
+  console.log(comment, content);
+  
+   dataBase('posts').where({login_id: login_id}).update({ comment, content })
+    .then(() => {
+      res.status(200).json({ message: 'Item updated successfully' });
+    })
+    .catch((err: any) => {
+      console.error(err);
+      res.status(500).json({ message: 'Error updating item' });
+    });
 })
